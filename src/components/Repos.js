f3f28f8263e2fec6a8809da2c352fa5a6635282d
Pavel Sorokin <moriel569@gmail.com>
2020-04-20 18:18:47 +0300

    Add working project v1.0

---
+++ src/components/Repos.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const Repos = ({ repos }) => {
+    console.log(repos)
+    return (
+        <React.Fragment>
+            {repos.map(repo => (
+                <div className="card mb-3" key={repo.id}>
+                    <div className="card-body">
+                        <h5>
+                            <a
+                                href={repo.html_url}
+                                rel="noopener noreferrer"
+                                target="blank"
+                            >{repo.name}</a>
+                        </h5>
+                    </div>
+                </div>
+            ))}
+        </React.Fragment>
+    )
+}
